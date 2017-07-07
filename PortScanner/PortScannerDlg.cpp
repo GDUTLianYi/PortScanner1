625b3a2f29d7f3025acb9ec8e074f265933b5157
lianyi <970026607@qq.com>
2017-07-07 22:41:56 +0800

    2017/7/7

--- PortScanner/PortScannerDlg.cpp
+++ PortScanner/PortScannerDlg.cpp
@@ -73,6 +73,7 @@ BEGIN_MESSAGE_MAP(CPortScannerDlg, CDialogEx)
 	ON_CBN_SELCHANGE(IDC_COMBO_THREAD, &CPortScannerDlg::OnCbnSelchangeComboThread)
 	ON_BN_CLICKED(IDC_BUTTON_EXCEL, &CPortScannerDlg::OnBnClickedButtonExcel)
 	ON_BN_CLICKED(IDC_BUTTON4, &CPortScannerDlg::OnBnClickedButton4)
+	ON_WM_DESTROY()
 END_MESSAGE_MAP()
 
 
@@ -110,29 +111,41 @@ BOOL CPortScannerDlg::OnInitDialog()
 	CListCtrl *pList;
 	// TODO: 在此添加额外的初始化代码
 	pIP=(CIPAddressCtrl*)GetDlgItem(IDC_IPADDRESS);
-	pList = (CListCtrl*)GetDlgItem(IDC_LIST_PORT);
-	pList->SetExtendedStyle(LVS_EX_FULLROWSELECT | LVS_EX_GRIDLINES);
-	pList->InsertColumn(0, "IP地址", 0, 180);
-	pList->InsertColumn(1, "端口地址", 0, 120);
+	HWND h1 = ::GetDlgItem(m_hWnd, IDC_LIST_PORT);
+	m_list.Attach(h1);
+	//pList = (CListCtrl*)GetDlgItem(IDC_LIST_PORT);
+	m_list.SetExtendedStyle(LVS_EX_FULLROWSELECT | LVS_EX_GRIDLINES);
+	m_list.InsertColumn(0, "IP地址", 0, 180);
+	m_list.InsertColumn(1, "端口地址", 0, 120);
 
-	CComboBox* pComb = (CComboBox*)GetDlgItem(IDC_COMBO1);
+	h1 = ::GetDlgItem(m_hWnd, IDC_IPADDRESS); m_IP1.SubclassWindow(h1);
+	h1 = ::GetDlgItem(m_hWnd, IDC_IPADDRESS1); m_IP2.SubclassWindow(h1);
+	//CComboBox* pComb = (CComboBox*)GetDlgItem(IDC_COMBO1);
+	h1 = ::GetDlgItem(m_hWnd, IDC_COMBO1); m_CombTime.SubclassWindow(h1);
+	m_CombTime.AddString("500");
+	m_CombTime.AddString("800");
+	m_CombTime.AddString("1000");
+	m_CombTime.AddString("1200");
+	m_CombTime.AddString("1400");
+	m_CombTime.AddString("5000");
+	m_CombTime.SetCurSel(3);
 
-	pComb->AddString("500");
-	pComb->AddString("800");
-	pComb->AddString("1000");
-	pComb->AddString("1200");
-	pComb->AddString("1400");
-	pComb->AddString("5000");
-	pComb->SetCurSel(3);
-	CComboBox* pCombThread = (CComboBox*)GetDlgItem(IDC_COMBO_THREAD);
-	pCombThread->AddString("32");
-	pCombThread->AddString("64");
-	pCombThread->AddString("128");
-	pCombThread->AddString("256");
-	pCombThread->AddString("512");
-	pCombThread->AddString("1024");
-	pCombThread->AddString("2048");
-	pCombThread->SetCurSel(3);
+	
+	//CComboBox* pCombThread = (CComboBox*)GetDlgItem(IDC_COMBO_THREAD);
+	h1 = ::GetDlgItem(m_hWnd, IDC_COMBO_THREAD); m_CombThread.SubclassWindow(h1);
+	m_CombThread.AddString("32");
+	m_CombThread.AddString("64");
+	m_CombThread.AddString("128");
+	m_CombThread.AddString("256");
+	m_CombThread.AddString("512");
+	m_CombThread.AddString("1024");
+	m_CombThread.AddString("2048");
+	m_CombThread.AddString("4096");
+	m_CombThread.AddString("8192");
+	
+	m_CombThread.SetCurSel(3);
+
+	h1 = ::GetDlgItem(m_hWnd, IDC_PROGRESS1); m_Progress.SubclassWindow(h1);
 	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
 }
 
@@ -224,21 +237,9 @@ DWORD WINAPI ThreadFunc(LPVOID pParam) {
 	server.sin_addr.s_addr = inet_addr(IP);
 	if (server.sin_addr.s_addr == INADDR_NONE) { return 0; }
 
-
-
-
-
-	/*
-	if (i % 100 == 0) {
-	CString str;
-	str.Format("%u", i);
-	AfxMessageBox(str);
-	}
-	*/
-
 	server.sin_port = htons(port);
 	if (WSAStartup(MAKEWORD(2, 0), &wsd)) { return 0; }
-	cClient = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	cClient = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); //创建一个客户端socket
 	if (cClient == INVALID_SOCKET) { return 0; }
 	//set Recv and Send time out
 	DWORD TimeOut = 3000; //设置发送超时6秒
@@ -283,44 +284,32 @@ DWORD WINAPI ThreadFunc(LPVOID pParam) {
 
 void CPortScannerDlg::OnBnClickedButton2()
 {
-	CListCtrl *pList = (CListCtrl*)GetDlgItem(IDC_LIST_PORT);
-	int nCount = pList->GetItemCount();
-	// Delete all of the items from the list view control.
-	for (int i = 0; i < nCount; i++)
-	{
-		pList->DeleteItem(0);
-	}
+	
+	//m_list.DeleteAllItems();
 
-
-	// TODO: 在此添加控件通知处理程序代码
-	CIPAddressCtrl *pIP = (CIPAddressCtrl*)GetDlgItem(IDC_IPADDRESS);
-	CIPAddressCtrl *pIP1 = (CIPAddressCtrl*)GetDlgItem(IDC_IPADDRESS1);
-	BYTE f[4] = { 10,21,32,111 }, g[4] = { 10,21,32,111 };
-	pIP->GetAddress(f[0], f[1], f[2], f[3]);
-	pIP1->GetAddress(g[0], g[1], g[2], g[3]);
+	m_IP1.GetAddress(f[0], f[1], f[2], f[3]);
+	m_IP2.GetAddress(g[0], g[1], g[2], g[3]);
 
 	int range = 1;
 	for (int r = 0; r < 4; r++)
 		range *= (g[r] - f[r] + 1);
 
-	CProgressCtrl *cpc = (CProgressCtrl*)GetDlgItem(IDC_PROGRESS1);
-	cpc->SetRange(0, range);
-	cpc->SetPos(0);
+	m_Progress.SetRange(0, range);
+	m_Progress.SetPos(0);
+
 	CEdit* pEdit = (CEdit*)GetDlgItem(IDC_EDIT1);
 	CString str; pEdit->GetWindowTextA(str);
 	int tmpxx = _ttoi(str);
 	UINT goalPort = tmpxx;
-	CComboBox* pComb = (CComboBox*)GetDlgItem(IDC_COMBO1);
-	pComb->GetWindowTextA(str);
-	int usetime= _ttoi(str);
 
-	const int size = 9999;
-	const UINT one = 256;
-	DWORD exitCode[size];
-	DWORD threadId[size];
-	HANDLE hThrds[size];
-	UINT port[size];
-	mypack pack[size];
+
+	
+	m_CombTime.GetWindowTextA(str);
+	int usetime = _ttoi(str);
+	m_CombThread.GetWindowTextA(str);
+	int col = _ttoi(str);
+
+	
 	CString res;
 	//UINT x = 6434;
 	
@@ -366,13 +355,14 @@ void CPortScannerDlg::OnBnClickedButton2()
 						//CString  tmp;
 						//tmp.Format(" %d", pack[f3].port);
 						//res.Append(tmp);
-						int ncount = pList->GetItemCount();
-						pList->InsertItem(ncount, pack[f3].IP);
+						int ncount = m_list.GetItemCount();
+						m_list.InsertItem(ncount, pack[f3].IP);
 						CString sport; sport.Format("%d", pack[f3].port);
-						pList->SetItemText(ncount, 1, sport);
+						m_list.SetItemText(ncount, 1, sport);
 					}
+					CloseHandle(hThrds[f3]);
 				}
-				cpc->SetPos(range);
+				m_Progress.SetPos(range);
 				UpdateWindow();
 			}
 		}
@@ -384,37 +374,26 @@ void CPortScannerDlg::OnBnClickedButton2()
 void CPortScannerDlg::OnBnClickedButton1()
 {
 	
-	CListCtrl *pList = (CListCtrl*)GetDlgItem(IDC_LIST_PORT);
-	int nCount = pList->GetItemCount();
-	// Delete all of the items from the list view control.
-	for (int i = 0; i < nCount; i++)
-	{
-		pList->DeleteItem(0);
-	}
-	CIPAddressCtrl *pIP = (CIPAddressCtrl*)GetDlgItem(IDC_IPADDRESS);
-	CIPAddressCtrl *pIP1 = (CIPAddressCtrl*)GetDlgItem(IDC_IPADDRESS1);
-	BYTE f[4] = { 10,21,32,111 }, g[4] = { 10,21,32,111 };
-	pIP->GetAddress(f[0], f[1], f[2], f[3]);
-	pIP1->GetAddress(g[0], g[1], g[2], g[3]);
+	
+	//m_list.DeleteAllItems();
+	m_IP1.GetAddress(f[0], f[1], f[2], f[3]);
+	m_IP2.GetAddress(g[0], g[1], g[2], g[3]);
 	int range = 1;
 	for (int r = 0; r < 4; r++)
 		range *= (g[r] - f[r] + 1);
 	
-	CProgressCtrl *cpc = (CProgressCtrl*)GetDlgItem(IDC_PROGRESS1);
-	cpc->SetRange(0, range);
-	cpc->SetPos(0);
-	CString str; 
-	CComboBox* pComb = (CComboBox*)GetDlgItem(IDC_COMBO1);
-	pComb->GetWindowTextA(str);
-	int usetime = _ttoi(str);
 
-	const int size = 9999;
-	const UINT one = 256;
-	DWORD exitCode[size];
-	DWORD threadId[size];
-	HANDLE hThrds[size];
-	UINT port[size];
-	mypack pack[size];
+	
+
+	CString str; 
+	
+	m_CombTime.GetWindowTextA(str);
+	int usetime = _ttoi(str);
+	m_CombThread.GetWindowTextA(str);
+	int col = _ttoi(str);
+
+	m_Progress.SetRange(0, range *max(1, 1024 / col));
+	m_Progress.SetPos(0);
 	CString res;
 	int cnt = 0;
 	for (int f0 = f[0]; f0 <= g[0]; f0++) {
@@ -422,55 +401,57 @@ void CPortScannerDlg::OnBnClickedButton1()
 			for (int f2 = f[2]; f2 <= g[2]; f2++) {
 
 				for (int f3 = f[3]; f3 <= g[3]; f3++) {
-					
+
 					CString szIP;
 
 					szIP.Format("%d.%d.%d.%d", f0, f1, f2, f3);
 					//szIP = "10.21.32.111";
-					for (int p = 0; p <= 1024; p++) {
+					for (int j = 0; j < max(1, 1024 / col); j++) {
+						for (int p = 0; p <= min(1023,col); p++) {
 
-						pack[p].port = p;
-						pack[p].time = usetime;
-						pack[p].IP = szIP;
+							pack[p].port = j*col+p;
+							pack[p].time = usetime;
+							pack[p].IP = szIP;
 
-						hThrds[p] = CreateThread(NULL, 0, ThreadFunc, &pack[p], 0, &threadId[p]);
-					}
-					for (;;)
-					{
-						int flag = 1;
+							hThrds[p] = CreateThread(NULL, 0, ThreadFunc, &pack[p], 0, &threadId[p]);
+						}
+						for (;;)
+						{
+							int flag = 1;
 
-						printf("Press any key to exit..\n");
-						//getch();
-						for (int p = 0; p <= 1024; p++) {
-							GetExitCodeThread(hThrds[p], &exitCode[p]);
-							if (exitCode[p] == STILL_ACTIVE) {
-								flag = 0;
+							printf("Press any key to exit..\n");
+							//getch();
+							for (int p = 0; p <  min(1023, col); p++) {
+								GetExitCodeThread(hThrds[p], &exitCode[p]);
+								if (exitCode[p] == STILL_ACTIVE) {
+									flag = 0;
+									break;
+								}
+							}
+
+							if (flag == 0) {
+								Sleep(1000);
+							}
+							else {
 								break;
 							}
 						}
+						for (int p = 0; p < min(1023, col); p++) {
 
-						if (flag == 0) {
-							Sleep(1000);
+							if (pack[p].port != 0) {
+								//	CString  tmp;
+								//	tmp.Format(" %d", pack[p].port);
+								//	res.Append(tmp);
+								int ncount = m_list.GetItemCount();
+								m_list.InsertItem(ncount, pack[p].IP);
+								CString sport; sport.Format("%d", pack[p].port);
+								m_list.SetItemText(ncount, 1, sport);
+							}
+							CloseHandle(hThrds[p]);
 						}
-						else {
-							break;
-						}
+						m_Progress.SetPos(++cnt);
+						UpdateWindow();
 					}
-					for (int p = 0; p <= 1024; p++) {
-
-						if (pack[p].port != 0) {
-							//	CString  tmp;
-							//	tmp.Format(" %d", pack[p].port);
-							//	res.Append(tmp);
-							int ncount = pList->GetItemCount();
-							pList->InsertItem(ncount, pack[p].IP);
-							CString sport; sport.Format("%d", pack[p].port);
-							pList->SetItemText(ncount, 1, sport);
-						}
-						
-					}
-					cpc->SetPos(++cnt);
-					UpdateWindow();
 				}
 			}
 		}
@@ -676,42 +657,25 @@ AfxMessageBox("ok");
 
 void CPortScannerDlg::OnBnClickedButton3()
 {
-	// TODO: 在此添加控件通知处理程序代码
-	CListCtrl *pList = (CListCtrl*)GetDlgItem(IDC_LIST_PORT);
-	int nCount = pList->GetItemCount();
-	// Delete all of the items from the list view control.
-	for (int i = 0; i < nCount; i++)
-	{
-		pList->DeleteItem(0);
-	}
-	CIPAddressCtrl *pIP = (CIPAddressCtrl*)GetDlgItem(IDC_IPADDRESS);
-	CIPAddressCtrl *pIP1 = (CIPAddressCtrl*)GetDlgItem(IDC_IPADDRESS1);
-	BYTE f[4] = { 10,21,32,111 }, g[4] = { 10,21,32,111 };
-	pIP->GetAddress(f[0], f[1], f[2], f[3]);
-	pIP1->GetAddress(g[0], g[1], g[2], g[3]);
+
+	//m_list.DeleteAllItems();
+	m_IP1.GetAddress(f[0], f[1], f[2], f[3]);
+	m_IP2.GetAddress(g[0], g[1], g[2], g[3]);
 
 	int range = 1;
 	for (int r = 0; r < 4; r++)
 		range *= (g[r] - f[r] + 1);
 
-	CProgressCtrl *cpc = (CProgressCtrl*)GetDlgItem(IDC_PROGRESS1);
-	cpc->SetRange(0, range * 64*4);
-	cpc->SetPos(0);
+	
 	CString str;
-	CComboBox* pComb = (CComboBox*)GetDlgItem(IDC_COMBO1);
-	pComb->GetWindowTextA(str);
+
+	m_CombTime.GetWindowTextA(str);
 	int usetime = _ttoi(str);
-	CComboBox* pCombThread = (CComboBox*)GetDlgItem(IDC_COMBO_THREAD);
-	pComb->GetWindowTextA(str);
+	m_CombThread.GetWindowTextA(str);
 	int col = _ttoi(str);
 	
-	const int size = 9999;
-	const UINT one = 256;
-	DWORD exitCode[size];
-	DWORD threadId[size];
-	HANDLE hThrds[size];
-	UINT port[size];
-	mypack pack[size];
+	m_Progress.SetRange(0, range * 65536 / col);
+	m_Progress.SetPos(0);
 	CString res;
 
 	int cnt = 0;
@@ -726,7 +690,7 @@ void CPortScannerDlg::OnBnClickedButton3()
 					szIP.Format("%d.%d.%d.%d", f0, f1, f2, f3);
 					//szIP = "10.21.32.111";
 				
-					for (int j = 0; j < 64*4; j++) {
+					for (int j = 0; j < 65536/col; j++) {
 						for (int p = 0; p < col; p++) {
 
 							pack[p].port = p +col * j;
@@ -762,13 +726,14 @@ void CPortScannerDlg::OnBnClickedButton3()
 								//CString  tmp;
 								//tmp.Format(" %d", pack[p].port);
 								//res.Append(tmp);
-								int ncount = pList->GetItemCount();
-								pList->InsertItem(ncount, pack[p].IP);
+								int ncount = m_list.GetItemCount();
+								m_list.InsertItem(ncount, pack[p].IP);
 								CString sport; sport.Format("%d", pack[p].port);
-								pList->SetItemText(ncount, 1, sport);
+								m_list.SetItemText(ncount, 1, sport);
 							}
+							CloseHandle(hThrds[p]);
 						}
-						cpc->SetPos(++cnt);
+						m_Progress.SetPos(++cnt);
 						UpdateWindow();
 					}
 
@@ -791,21 +756,15 @@ void CPortScannerDlg::OnCbnSelchangeComboThread()
 void CPortScannerDlg::OnBnClickedButtonExcel()
 {
 	// TODO: 在此添加控件通知处理程序代码
-	CFileDialog fdlg(false,"xls",NULL);
+	CFileDialog fdlg(false,"txt",NULL);
 	INT_PTR rt = fdlg.DoModal();
 	CListCtrl *plist = (CListCtrl*)GetDlgItem(IDC_LIST_PORT);
 	if (IDOK == rt) {
 		CString path = fdlg.GetPathName();
 		CString name = fdlg.GetFileName();
 		//AfxMessageBox("你点了保存");
-		int ncount = plist->GetItemCount();
-		CString str;
-		for (int i = 0; i < ncount; i++) {
-			CString tmp = plist->GetItemText(i, 0);
-			str.Append(" " + tmp);
-			tmp = plist->GetItemText(i,1);
-			str.Append(" " + tmp);
-		}
+		
+		
 		CFile f; CFileException e;
 
 		if (!f.Open(path, CFile::modeCreate | CFile::modeWrite, &e)) {
@@ -813,9 +772,24 @@ void CPortScannerDlg::OnBnClickedButtonExcel()
 			
 		}
 		else {
-			char s[4096]="1233456789";
-			//memcpy(s, str, str.GetLength());
-			f.Write(s, sizeof s);
+			//char s[4096]="1233456789";
+			char s[1024], l[1] = {'\r\n'};
+			int ncount = m_list.GetItemCount();
+			for (int i = 0; i < ncount; i++) {
+				CString str1 = m_list.GetItemText(i, 0);
+				CString str2 = m_list.GetItemText(i, 1);
+				CString str = str1 + ' '+str2;
+				memset(s, 0, sizeof s);
+				memcpy(s, str, str.GetLength());
+				f.Write(s, sizeof s);
+				//str = m_list.GetItemText(i, 1);
+				//str.Append("\r\n");
+				//memset(s, 0, sizeof s);
+				//memcpy(s, str, str.GetLength());
+				//f.Write(s, sizeof s);
+				
+			}
+		
 			f.Flush();
 			f.Close();
 		}
@@ -829,5 +803,18 @@ void CPortScannerDlg::OnBnClickedButtonExcel()
 
 void CPortScannerDlg::OnBnClickedButton4()
 {
-	
+	m_list.DeleteAllItems();
+}
+
+
+void CPortScannerDlg::OnDestroy()
+{
+	CDialogEx::OnDestroy();
+	m_list.Detach();
+	m_IP1.UnsubclassWindow();
+	m_IP2.UnsubclassWindow();
+	m_CombTime.UnsubclassWindow();
+	m_CombThread.UnsubclassWindow();
+	m_Progress.UnsubclassWindow();
+	// TODO: 在此处添加消息处理程序代码
 }
--- PortScanner/PortScannerDlg.h
+++ PortScanner/PortScannerDlg.h
@@ -12,13 +12,23 @@ class CPortScannerDlg : public CDialogEx
 // 构造
 public:
 	CPortScannerDlg(CWnd* pParent = NULL);	// 标准构造函数
-
+	//自定义数据包
 	struct mypack {
 		CString IP;
 		UINT port;
 		int time;
 	};
-	
+	CListCtrl m_list;    
+	CIPAddressCtrl m_IP1, m_IP2;
+	CComboBox m_CombTime ,m_CombThread;
+	CProgressCtrl m_Progress;
+	BYTE f[4] = { 0 }, g[4] = { 0 };
+	const static int size = 8199;
+	DWORD exitCode[size];
+	DWORD threadId[size];
+	HANDLE hThrds[size];
+	UINT port[size];
+	mypack pack[size];
 // 对话框数据
 #ifdef AFX_DESIGN_TIME
 	enum { IDD = IDD_PORTSCANNER_DIALOG };
@@ -40,11 +50,12 @@ protected:
 	DECLARE_MESSAGE_MAP()
 public:
 	afx_msg void OnBnClickedButtonDo();
-	afx_msg void OnBnClickedButton1();
-	afx_msg void OnBnClickedButton2();
-	afx_msg void OnEnChangeEdit1();
-	afx_msg void OnBnClickedButton3();
-	afx_msg void OnCbnSelchangeComboThread();
-	afx_msg void OnBnClickedButtonExcel();
+	afx_msg void OnBnClickedButton1();  //对应于扫描特定端口功能的实现
+	afx_msg void OnBnClickedButton2();  //对应于扫描常用端口功能的实现
+	afx_msg void OnEnChangeEdit1();      //对应于特定端口输入框
+	afx_msg void OnBnClickedButton3();   //对应于扫描常用端口功能的实现
+	afx_msg void OnCbnSelchangeComboThread(); //对应于线程列表框
+	afx_msg void OnBnClickedButtonExcel(); //对应于另存为
 	afx_msg void OnBnClickedButton4();
+	afx_msg void OnDestroy();
 };
